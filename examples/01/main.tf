06520060c76cba4dd66839db5f5e6cf0287fc8b4
Tomer Admon <Tomer.a@qualisystems.com>
2023-04-30 11:35:29 +0300

    update modules

--- examples/01/main.tf
+++ examples/01/main.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source  = "/modules/private_network"
+  source  = "./modules/private_network"
   region  = var.region
   network = var.network
   subnet  = var.subnet
@@ -7,18 +7,18 @@ module "network" {
 }
 
 module "keypair" {
-  source  = "/modules/ssh_keypair"
+  source  = "./modules/ssh_keypair"
   keypair = var.keypair
 }
 
 module "bastion" {
-  source     = "/modules/instance_simple"
+  source     = "./modules/instance_simple"
   depends_on = [module.keypair, module.network]
   instance   = var.bastion
 }
 
 module "floatip" {
-  source     = "/modules/floating_ip"
+  source     = "./modules/floating_ip"
   depends_on = [module.bastion]
   floatip = {
     region       = var.region
