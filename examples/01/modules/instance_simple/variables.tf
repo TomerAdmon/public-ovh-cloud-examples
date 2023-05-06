733b4e3bddd07cefc6b15c0ce21ea17326d390c9
Tomer Admon <Tomer.a@qualisystems.com>
2023-05-06 15:03:20 +0300

    .

--- examples/01/modules/instance_simple/variables.tf
+++ examples/01/modules/instance_simple/variables.tf
@@ -4,7 +4,6 @@ variable "instance" {
   description = "Instance Parameters"
   type = object({
     region       = string
-    network_name = string
     keypair_name = string
     name         = string
     flavor       = string
