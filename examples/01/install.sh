3a9a90c8b426d5578e5fa85fca767f4cfc6be381
Tomer Admon <Tomer.a@qualisystems.com>
2023-05-06 18:26:51 +0300

    .

--- examples/01/install.sh
+++ examples/01/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash 
-export ENDPOINT=%s
+
+END=$(echo %s | sed 's~:[[:digit:]]\+~~g')
+
+export ENDPOINT=$END
 export PASSWORD=%s
 
 echo this is from the script itself!!
@@ -7,10 +10,10 @@ echo $ENDPOINT
 echo $PASSWORD
 echo end of script prints
 
-sudo apt-get install git -y
-sudo apt-get install nodejs -y
-sudo apt install npm -y
-git clone https://github.com/TomerAdmon/express-mysql-todo.git
-cd express-mysql-todo
-sudo npm install
-nohup node server.js &
+# sudo apt-get install git -y
+# sudo apt-get install nodejs -y
+# sudo apt install npm -y
+# git clone https://github.com/TomerAdmon/express-mysql-todo.git
+# cd express-mysql-todo
+# sudo npm install
+# nohup node server.js &
