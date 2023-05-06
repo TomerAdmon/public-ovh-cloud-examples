abb9ab59e183bfe39d2d4c5a614fc87a3a51348c
Tomer Admon <Tomer.a@qualisystems.com>
2023-05-06 18:28:38 +0300

    .

--- examples/01/install.sh
+++ examples/01/install.sh
@@ -10,10 +10,10 @@ echo $ENDPOINT
 echo $PASSWORD
 echo end of script prints
 
-# sudo apt-get install git -y
-# sudo apt-get install nodejs -y
-# sudo apt install npm -y
-# git clone https://github.com/TomerAdmon/express-mysql-todo.git
-# cd express-mysql-todo
-# sudo npm install
-# nohup node server.js &
+sudo apt-get install git -y
+sudo apt-get install nodejs -y
+sudo apt install npm -y
+git clone https://github.com/TomerAdmon/express-mysql-todo.git
+cd express-mysql-todo
+sudo npm install
+nohup node server.js &
